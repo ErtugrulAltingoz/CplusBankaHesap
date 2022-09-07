5187d7b7c811956f5da4f309448304f7c04252b2
Ertuğrul Altıngöz <altingozertugrul@gmail.com>
2022-09-07 04:36:25 +0300

    Banka Hesabı
    Miras alma

---
+++ Banka Hesabı/Banka Hesabı.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+#include "account.h"
+
+
+
+void  main()
+{
+    insan insan1("Ertugrul Altingoz", 25);
+    insan1.printİnsan();
+    hesap hesap1(10000, "Ertugrul Altingoz", 25);
+    hesap1.printHesap();
+    cout << "\n";
+    hesap1.cekHesap(3000);
+    hesap1.printHesap();
+    cout << "\n";
+    hesap1.cekHesap(8000);
+    hesap1.printHesap();
+    cout << "\n";
+    hesap1.yatirHesap(1000);
+    hesap1.printHesap();
+}
+
---
+++ Banka Hesabı/account.h
@@ -0,0 +1,30 @@
+#pragma once
+#include<iostream>
+
+class insan {
+private: 
+	std::string isim;
+	int yas;
+public:
+	insan();
+	insan(std::string, int);
+	void setIsim(std::string);
+	std::string  getIsim();
+	void setYas(int);
+	int getYas();
+	void printİnsan (); 
+};
+
+class hesap : public insan {
+private:
+	int hesapBakiyesi; 
+public:
+	hesap();
+	hesap(int,std::string,int);
+	void setHesap(int);
+	int getHesap();
+	void printHesap();
+	void yatirHesap(int);
+	void cekHesap(int);
+	 
+};
